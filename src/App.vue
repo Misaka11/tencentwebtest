ca9ed46c322727f9d08a922723101ea601abae51
Misaka11 <853189424@qq.com>
2022-06-02 14:11:38 +0800

    lwa

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,88 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a id="LoginWithAmazon">
+      <img
+        border="0"
+        alt="Login with Amazon"
+        src="https://images-na.ssl-images-amazon.com/images/G/01/lwa/btnLWA_gold_156x32.png"
+        width="156"
+        height="32"
+      />
+    </a>
+    <br />
+    <a id="Logout">Logout</a>
+    <br />
+    {{ rdata }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      rdata: "no data",
+    };
+  },
+  created() {
+    window.onAmazonLoginReady = function () {
+      amazon.Login.setClientId(
+        "amzn1.application-oa2-client.292775ca65044ae69cae9783da4848b5"
+      );
+      console.log("hello, im ready");
+      document.getElementById("LoginWithAmazon").onclick = function () {
+        this.rdata = "click on LoginWithAmazon";
+        setTimeout(window.doLogin, 1);
+        return false;
+      };
+      window.doLogin = function () {
+        var options = {};
+        options.scope = "profile";
+        options.pkce = true;
+        amazon.Login.authorize(options, function (response) {
+          if (response.error) {
+            alert("oauth error " + response.error);
+            return;
+          }
+          amazon.Login.retrieveToken(response.code, function (response) {
+            if (response.error) {
+              alert("oauth error " + response.error);
+              return;
+            }
+            amazon.Login.retrieveProfile(
+              response.access_token,
+              function (response) {
+                alert("Hello, " + response.profile.Name);
+                alert(
+                  "Your e-mail address is " + response.profile.PrimaryEmail
+                );
+                alert("Your unique ID is " + response.profile.CustomerId);
+                if (window.console && window.console.log)
+                  window.console.log(response);
+              }
+            );
+          });
+        });
+      };
+      document.getElementById("Logout").onclick = function () {
+        amazon.Login.logout();
+      };
+    };
+    (function (d) {
+      var a = d.createElement("script");
+      a.type = "text/javascript";
+      a.async = true;
+      a.id = "amazon-login-sdk";
+      a.src = "https://assets.loginwithamazon.com/sdk/na/login1.js";
+      d.getElementById("amazon-root").appendChild(a);
+    })(document);
+  },
+};
 </script>
 
 <style>
